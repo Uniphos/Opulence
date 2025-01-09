40ac444d8baec7c6100a22b3b7df901a937ee539
UniPhos <victor.ribas6@gmail.com>
2025-01-08 21:16:53 -0500

    changing configs

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
-import ReactDom from 'react-router-dom'
+import ReactDOM from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-ReactDom.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
